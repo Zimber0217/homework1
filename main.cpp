0560488290b436163d1e26dc4da477e6683c2327
Zimber <baevk0217@gmail.com>
2024-03-14 17:23:19 +0300

    homework1

--- main.cpp
+++ main.cpp
@@ -1,65 +1,63 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
 #include <cstring>
 
-using namespace std;
-
-bool hasOption(const string& option, const vector<string>& args) {
-    for (const string& arg : args) {
-        if (arg == option) {
-            return true;
-        }
+int countLines(std::ifstream& file) {
+    int lines = 0;
+    std::string line;
+    while (std::getline(file, line)) {
+        lines++;
     }
-    return false;
+    return lines;
 }
 
-int countLines(const string& filename) {
-    ifstream file(filename);
-    int count = 0;
-    string line;
-    while (getline(file, line)) {
-        count++;
-    }
-    return count;
-}
-
-int countWords(const string& filename) {
-    ifstream file(filename);
-    int count = 0;
-    string word;
+int countWords(std::ifstream& file) {
+    int words = 0;
+    std::string word;
     while (file >> word) {
-        count++;
+        words++;
     }
-    return count;
+    return words;
 }
 
-int countBytes(const string& filename) {
-    ifstream file(filename, ios::binary | ios::ate);
+int countBytes(std::ifstream& file) {
+    file.seekg(0, std::ios::end);
     return file.tellg();
 }
 
 int main(int argc, char* argv[]) {
-    vector<string> args(argv + 1, argv + argc);
-    bool showLines = hasOption("-l", args) || hasOption("--lines", args);
-    bool showWords = hasOption("-w", args) || hasOption("--words", args);
-    bool showBytes = hasOption("-c", args) || hasOption("--bytes", args);
+    int lines = 0;
+    int words = 0;
+    int bytes = 0;
 
-    vector<string> filenames;
-    for (const string& arg : args) {
-        if (arg != "-l" && arg != "-c" && arg != "-w" && arg != "--lines" && arg != "--bytes" && arg != "--words") {
-            filenames.push_back(arg);
+    bool countLinesOption = false;
+    bool countWordsOption = false;
+    bool countBytesOption = false;
+
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--lines") == 0) {
+            countLinesOption = true;
+        } else if (strcmp(argv[i], "-w") == 0 || strcmp(argv[i], "--words") == 0) {
+            countWordsOption = true;
+        } else if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--bytes") == 0) {
+            countBytesOption = true;
+        } else {
+            std::ifstream file(argv[i]);
+            if (!file.is_open()) {
+                std::cerr << "Error: Unable to open file " << argv[i] << std::endl;
+                return 1;
+            }
+            if (countLinesOption) {
+                lines = countLines(file);
+            }
+            if (countWordsOption) {
+                words = countWords(file);
+            }
+            if (countBytesOption) {
+                bytes = countBytes(file);
+            }
+            file.close();
         }
     }
 
-    for (const string& filename : filenames) {
-        int lines = showLines ? countLines(filename) : 0;
-        int words = showWords ? countWords(filename) : 0;
-        int bytes = showBytes ? countBytes(filename) : 0;
-
-        cout << lines << " " << words << " " << bytes << " " << filename << endl;
-    }
-
-    return 0;
-}
+    std::cout << lines << " " << words << " " << bytes;
